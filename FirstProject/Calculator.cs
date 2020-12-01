5d0b8f37c6ddd08b26f4146d9c58fae78846790d
BogdanPetre27 <bogdanertep@gmail.com>
2020-12-01 15:17:12 +0200

    First Commit

---
+++ FirstProject/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PrimulProiect
+{
+    static class Calculator
+    {
+        public static int Sum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public static double Divide(int firstNumber, int secondNumber)
+        {
+            try
+            {
+                // Încearcă
+                return (double) firstNumber / secondNumber;
+            }
+            catch (DivideByZeroException)
+            {
+                // Dacă se aruncă DivideByZeroException fă asta
+                Console.WriteLine("Diviziunea nu a fost posibila, secondNumber a fost 0.");
+                return 0;
+            }            
+        }
+
+        public static int Multiply(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public static int Difference(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+    }
+}
---
+++ FirstProject/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace PrimulProiect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.WriteLine("Vă rog să selectati operatia:");
+                Console.WriteLine("1. Adunare");
+                Console.WriteLine("2. Scădere");
+                Console.WriteLine("3. Înmultire");
+                Console.WriteLine("4. Împărtire");
+                Console.WriteLine("5. Iesire aplicatie");
+
+                string input = Console.ReadLine();
+
+                int operation, firstNumber, secondNumber;
+
+                int.TryParse(input, out operation);
+
+                if (operation == 5)
+                {
+                    Console.WriteLine("Calculatorul a fost oprit.");
+                    return;
+                }
+
+                Console.WriteLine("Va rog sa introduceti primul numar..");
+                int.TryParse(Console.ReadLine(), out firstNumber);
+
+                Console.WriteLine("Va rog sa introduceti al doilea numar..");
+                int.TryParse(Console.ReadLine(), out secondNumber);
+
+                double result = 0;
+
+                switch (operation)
+                {
+                    case 1:
+                        result = Calculator.Sum(firstNumber, secondNumber);
+                        break;
+                    case 2:
+                        result = Calculator.Difference(firstNumber, secondNumber);
+                        break;
+                    case 3:
+                        result = Calculator.Multiply(firstNumber, secondNumber);
+                        break;
+                    case 4:
+                        result = Calculator.Divide(firstNumber, secondNumber);
+                        break;
+                }
+
+                Console.WriteLine("Rezultatul este : " + result);
+            }
+        }
+    }
+}
